cbc4f22fb358c86a2b1782257f3ad65e33ddf1bd
Andrew Coon <thenran@gmail.com>
2021-04-20 10:15:27 -0400

    Resolved issues with loading db.json data and rendering/saving new notes list. Need to implement note ids.

--- server.js
+++ server.js
@@ -7,13 +7,14 @@ const PORT = 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static('public'));
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, './public/index.html')));
 
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
-
-app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, 'notes.html')));
+app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, './public/notes.html')));
 
 app.get('/api/notes', (req, res) => {
-  let notes = fs.readFileSync('./db/db.json');
+  let notes = JSON.parse(fs.readFileSync('./db/db.json', 'utf-8'));
+  console.log(notes);
   res.json(notes);
 });
 
@@ -21,11 +22,11 @@ app.post('/api/notes', (req, res) => {
   const newNote = req.body;
 
   // Read current db file and add new note to it
-  const notesdb = fs.readFileSync('./db/db.json');
+  const notesdb = JSON.parse(fs.readFileSync('./db/db.json', 'utf-8'));
   notesdb.push(newNote);
-  fs.writeFileSync('./db/db.json', notesdb);
-
-  res.json(newNote);
+  fs.writeFileSync('./db/db.json', JSON.stringify(notesdb));
+  console.log(notesdb);
+  res.json(notesdb);
 })
 
 app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
