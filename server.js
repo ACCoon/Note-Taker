9676c97d594012a47ab7dbe6bd7716eb7e1d126d
Andrew Coon <thenran@gmail.com>
2021-04-20 14:48:31 -0400

    Added id property to notes, delete route added and functional

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const express = require('express');
 const path = require('path');
+const { v1: uuidv1 } = require('uuid');
 
 const app = express();
 const PORT = 3000;
@@ -18,8 +19,15 @@ app.get('/api/notes', (req, res) => {
   res.json(notes);
 });
 
+app.delete('/api/notes/:id', (req, res) => {
+  const notes = JSON.parse(fs.readFileSync('./db/db.json', 'utf-8'));
+  const filterNotes = notes.filter(note => note.id !== req.params.id );
+  fs.writeFileSync('./db/db.json', JSON.stringify(filterNotes));
+  res.json(filterNotes);
+});
+
 app.post('/api/notes', (req, res) => {
-  const newNote = req.body;
+  const newNote = { ...req.body, id: uuidv1() };
 
   // Read current db file and add new note to it
   const notesdb = JSON.parse(fs.readFileSync('./db/db.json', 'utf-8'));
